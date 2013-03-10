a0e2fc3e2bf17fe5665612c19e2cdce07484b166
Kai Sellgren <kaisellgren@gmail.com>
2013-03-10 17:02:53 +0200

    Added sorting.

--- lib/data_grid.dart
+++ lib/data_grid.dart
@@ -1,9 +1,17 @@
 import 'dart:html';
 import 'dart:json';
 import 'dart:async';
+import 'dart:collection';
 
 import 'package:web_ui/web_ui.dart';
 import 'package:web_ui/watcher.dart' as watchers;
+
+class ColumnSort {
+  String id;
+  bool descending = false;
+
+  ColumnSort(this.id);
+}
 
 class MangoDataGrid extends WebComponent {
   List columns;
@@ -11,6 +19,8 @@ class MangoDataGrid extends WebComponent {
 
   int width;
   int height;
+
+  List<ColumnSort> columnSorts = [];
 
   inserted() {
     Timer.run(doLayout);
@@ -55,4 +65,67 @@ class MangoDataGrid extends WebComponent {
     _root.query('.mango-data-grid-header-table').style.width = '${totalWidth}px';
     _root.query('.mango-data-grid-data-table').style.width = '${totalWidth}px';
   }
+
+  sortBy(String id) {
+    // See if the colum is already sorted.
+    ColumnSort sort = columnSorts.firstMatching((c) => c.id == id, orElse: () => null);
+
+    if (sort == null) {
+      sort = new ColumnSort(id);
+
+      columnSorts.add(sort);
+    } else {
+      sort.descending = !sort.descending;
+
+      if (sort.descending == false) columnSorts.remove(sort); // Pressing a header thrice, removes the sort for the header.
+    }
+
+    _sort();
+  }
+
+  _sort() {
+    int compareFields(a, b) {
+      if (a == null || b == null) return 0;
+
+      // Numbers and dates can be compared with < and > operators.
+      if ((a is num && b is num) || (a is DateTime && b is DateTime)) {
+        if (a < b) return -1;
+        else if (a == b) return 0;
+        else return 1;
+      }
+
+      // Strings can be compared with .compareTo().
+      if (a is String && b is String) return a.compareTo(b);
+
+      // Booleans are straight-forward.
+      if (a is bool && b is bool) {
+        if (a == false && b == true) return -1;
+        else if (a == true && b == false) return 1;
+        else return 0;
+      }
+
+      // Generic.
+      if (a < b) return -1;
+      else if (a > b) return 1;
+      else return 0;
+    }
+
+    columnSorts.reversed.forEach((ColumnSort sort) {
+      rows.sort((left, right) {
+        var result = compareFields(left[sort.id], right[sort.id]);
+
+        if (sort.descending) return result * -1;
+
+        return result;
+      });
+    });
+
+    watchers.dispatch();
+
+    doLayout();
+  }
+
+  bool isColumnSorted(String id) => columnSorts.firstMatching((c) => c.id == id, orElse: () => null) != null;
+
+  bool isColumnAscendingSorted(String id) => !columnSorts.firstMatching((c) => c.id == id).descending;
 }
